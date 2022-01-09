91b08f1a49317622600235ee9523e4f03dcfa286
Piotr Świstowski <swistekxd@gmail.com>
2022-01-09 23:20:37 +0100

    manage users form admin panel

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -104,4 +104,78 @@ const updateUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerUser, updateUserProfile };
+//@desc get all users profile
+//@route GET / api/ users
+//@acess PRIVATE/admin
+
+const getUsers = asyncHandler(async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+});
+
+//@desc delete user
+//@route DELETE  API USERS IS
+//@acess PRIVATE/admin
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    await user.remove();
+    res.json({ message: 'user removed' });
+  } else {
+    res.status(404);
+    throw new Error('user not found');
+  }
+});
+
+//@desc get user by id
+//@route GET / api/ users / id
+//@acess PRIVATE/admin
+
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password');
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404);
+    throw new Error('user not found');
+  }
+});
+
+//@desc Update user
+//@route PUT / api/ users / id
+//@acess PRIVATE/ ADMIN
+
+const updateUser = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    user.isAdmin = req.body.isAdmin;
+
+    const updateUser = await user.save();
+
+    res.json({
+      _id: updateUser._id,
+      name: updateUser.name,
+      email: updateUser.email,
+      isAdmin: updateUser.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+});
+
+export {
+  authUser,
+  getUserProfile,
+  registerUser,
+  updateUserProfile,
+  getUsers,
+  deleteUser,
+  getUserById,
+  updateUser,
+};
